b3f533490f5162b00727b4b15aa1bda4f42d3d66
Alijah LeMay <36865734+Alijah-LeMay@users.noreply.github.com>
2026-04-10 00:50:02 -0700

    Update solution.js
    Final solution

--- solution.js
+++ solution.js
@@ -4,47 +4,43 @@ function solution(sentences, words) {
   let result=[]
   
   for(let i=0; i<sentences.length;i++) {
-    console.log(sentences[i],words[i])
-    let newSentence = []
+    let newSentence 
      let start_pos= sentences[i].toLowerCase().indexOf(words[i].toLowerCase())
     let match_indices=[]
     
      while(start_pos !== -1) {
-      console.log(words[i])
       match_indices.push(start_pos)
-      
       start_pos= sentences[i].toLowerCase().indexOf(words[i].toLowerCase(), start_pos+words[i].length)
-      
      } 
-     
-     if() {
-      
-     }
+        let currentString=''
+          let currentSentence = sentences[i]
      for(let idx=0;idx <match_indices.length; idx++) {
-        console.log(sentences[i][match_indices[idx]])
-        
         if(sentences[i][match_indices[idx]] === sentences[i][match_indices[idx]].toUpperCase()) {
-          console.log("uppercase char")
-          let newWord = words[i].split("").reverse().join("")
           
-          console.log(newWord) 
-          sentences[i].replace(words[i],newWord) 
+          let firstPart=currentSentence.slice(0,match_indices[idx])
+          let secondPart = currentSentence.slice(match_indices[idx] +words[i].length)
+          let myWord = words[i].split("").reverse().join("")
+          let first = myWord.slice(0,1).toUpperCase()
+          let last =myWord.slice(1,myWord.length)
+          currentString = firstPart + first+last + secondPart         
           } else {
-          console.log("lowercase char")
-          let newWord = words[i].split("").reverse().join("")
-          console.log(newWord)  
-          newSentence.push(newWord)      
+          let firstPart=currentSentence.slice(0,match_indices[idx])
+          let secondPart = currentSentence.slice(match_indices[idx] +words[i].length)
+          let myWord = words[i].split("").reverse().join("")
+          let first = myWord.slice(0,1)
+          let last =myWord.slice(1,myWord.length)
+           currentString = firstPart + first+last + secondPart         
 
         }
+        currentSentence=currentString
+        newSentence=currentString
+     }
+     
+     if(match_indices.length ===0) {
+      newSentence = sentences[i]
      }
      result.push(newSentence)
-     
-     
-    
-     
   }
- 
-  
   console.log(result)
   return result
   
