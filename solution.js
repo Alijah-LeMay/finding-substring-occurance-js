0dc5e71e503327b384578c5fe188d6476d247815
Alijah LeMay <36865734+Alijah-LeMay@users.noreply.github.com>
2026-04-10 00:53:46 -0700

    Update solution.js
    Touchup

--- solution.js
+++ solution.js
@@ -1,6 +1,5 @@
 function solution(sentences, words) {
   // TODO: implement the solution
-  // console.log(sentences)
   let result=[]
   
   for(let i=0; i<sentences.length;i++) {
@@ -17,19 +16,21 @@ function solution(sentences, words) {
      for(let idx=0;idx <match_indices.length; idx++) {
         if(sentences[i][match_indices[idx]] === sentences[i][match_indices[idx]].toUpperCase()) {
           
-          let firstPart=currentSentence.slice(0,match_indices[idx])
-          let secondPart = currentSentence.slice(match_indices[idx] +words[i].length)
+          let firstSentencePart=currentSentence.slice(0,match_indices[idx])
+          let secondSentencePart = currentSentence.slice(match_indices[idx] +words[i].length)
           let myWord = words[i].split("").reverse().join("")
-          let first = myWord.slice(0,1).toUpperCase()
-          let last =myWord.slice(1,myWord.length)
-          currentString = firstPart + first+last + secondPart         
+          let firstHalf = myWord.slice(0,1).toUpperCase()
+          let lastHalf =myWord.slice(1,myWord.length)
+          
+          currentString = firstSentencePart + firstHalf+lastHalf + secondSentencePart         
           } else {
-          let firstPart=currentSentence.slice(0,match_indices[idx])
-          let secondPart = currentSentence.slice(match_indices[idx] +words[i].length)
+          let firstSentencePart=currentSentence.slice(0,match_indices[idx])
+          let secondSentencePart = currentSentence.slice(match_indices[idx] +words[i].length)
           let myWord = words[i].split("").reverse().join("")
-          let first = myWord.slice(0,1)
-          let last =myWord.slice(1,myWord.length)
-           currentString = firstPart + first+last + secondPart         
+          let firstHalf = myWord.slice(0,1)
+          let lastHalf = myWord.slice(1,myWord.length)
+          
+          currentString = firstSentencePart + firstHalf+lastHalf + secondSentencePart         
 
         }
         currentSentence=currentString
